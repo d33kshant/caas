c49db48a2a6602b3f98e1dc9063a117e09d11c32
Deekshant Yadav <d33kshant@gmail.com>
2024-06-29 23:00:36 +0530

    feat: imported global bull module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FilesModule } from './files/files.module';
 import { CompressionModule } from './compression/compression.module';
+import { BullModule } from '@nestjs/bull';
+import { REDIS_HOST, REDIS_PASS, REDIS_PORT } from './constants';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get<string>(REDIS_HOST),
+          port: configService.get<number>(REDIS_PORT),
+          password: configService.get<string>(REDIS_PASS),
+        }
+      })
     }),
     FilesModule,
     CompressionModule,
