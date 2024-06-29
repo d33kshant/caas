e78fa64e459fe6a4c34b395ad1b118173f01b141
Deekshant Yadav <d33kshant@gmail.com>
2024-06-29 22:54:32 +0530

    feat: implemented compress function

--- src/compression/compression.service.ts
+++ src/compression/compression.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream, unlink } from 'fs';
+import { MULTER_DEST } from 'src/constants';
 
 @Injectable()
-export class CompressionService {}
+export class CompressionService {
+  constructor(private readonly configService: ConfigService) {}
+
+  compress(file: string) {
+    const inputPath = `${this.configService.get<string>(MULTER_DEST)}${file}`;
+    const outputPath = `${this.configService.get<string>(MULTER_DEST)}${file}.gz`;
+
+    const gzip = createGzip();
+    const input = createReadStream(inputPath);
+    const output = createWriteStream(outputPath);
+
+    input.pipe(gzip).pipe(output);
+
+    output.on("close", function() {
+      const logger = new Logger(CompressionService.name);
+      logger.log(`Compressed ${file}`);
+      
+      unlink(inputPath, function(error) {
+        if (error) {
+          logger.error(`Failed to delete ${file}`);
+          return;
+        }
+        logger.log(`Deleted ${file}`);
+      })
+    })
+
+    output.on("error", function(error) {
+      const logger = new Logger(CompressionService.name);
+      logger.log(`Failed to compress ${file}`);
+      logger.error(error.message);
+    })
+  }
+}
