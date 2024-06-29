6a0693b68e1c3f525c4e5c28f03a0b701c6e54bc
Deekshant Yadav <d33kshant@gmail.com>
2024-06-29 18:05:28 +0530

    feat: imported multer module

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { diskStorage } from 'multer';
 
 @Module({
+  imports: [
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        storage: diskStorage({
+          destination: configService.get<string>('MULTER_DEST'),
+        })
+      }),
+    }),
+  ],
   controllers: [FilesController],
   providers: [FilesService]
 })
