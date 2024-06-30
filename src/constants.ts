f943a3c028979b112d2445d3dfe0ec1b642b5009
Deekshant Yadav <d33kshant@gmail.com>
2024-06-30 16:56:52 +0530

    feat: added custom filename

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const FILES_QUEUE = 'files-queue';
 export const MULTER_DEST = 'MULTER_DEST';
 
 export const PUBLIC_DEST = 'PUBLIC_DEST';
+export const PUBLIC_ROOT = 'PUBLIC_ROOT';
 export const STATIC_ROOT = 'STATIC_ROOT';
 
 export const REDIS_HOST = 'REDIS_HOST';
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -8,6 +8,7 @@ import { BullModule } from '@nestjs/bull';
 import { FILES_QUEUE, MULTER_DEST } from 'src/constants';
 import { FilesProcessor } from './files.processor';
 import { CompressionModule } from 'src/compression/compression.module';
+import { randomUUID } from 'crypto';
 
 @Module({
   imports: [
@@ -17,6 +18,11 @@ import { CompressionModule } from 'src/compression/compression.module';
       useFactory: async (configService: ConfigService) => ({
         storage: diskStorage({
           destination: configService.get<string>(MULTER_DEST),
+          filename: function(_, file, callback) {
+            const originalname = file.originalname.replaceAll(' ', '_');
+            const filename = `${randomUUID()}_${originalname}`;
+            callback(null, filename);
+          }
         })
       }),
     }),
