2511e6337e3d0ae01114258927addc891697751c
Deekshant Yadav <d33kshant@gmail.com>
2024-06-30 15:35:52 +0530

    feat: added serve-static for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FilesModule } from './files/files.module';
 import { CompressionModule } from './compression/compression.module';
 import { BullModule } from '@nestjs/bull';
-import { REDIS_HOST, REDIS_PASS, REDIS_PORT } from './constants';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_DEST, REDIS_HOST, REDIS_PASS, REDIS_PORT, STATIC_ROOT } from './constants';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -22,6 +24,14 @@ import { REDIS_HOST, REDIS_PASS, REDIS_PORT } from './constants';
           password: configService.get<string>(REDIS_PASS),
         }
       })
+    }),
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ([{
+        rootPath: join(process.cwd(), configService.get<string>(PUBLIC_DEST)),
+        serveRoot: configService.get<string>(STATIC_ROOT),
+      }])
     }),
     FilesModule,
     CompressionModule,
